da149105583cd02b5a92953a2c8e931b050ca9cb
Kenny H <kennyhuynh1198@gmail.com>
2018-09-05 10:41:08 +1000

    Created BoardTile() class. Added public function initializeBoard() to class ChessBoard(). In BoardTile() class: - Constructor for BoardTile with parameters (int) xPos, (int) yPos, (char) colour. - Accessor + Mutator for each of these parameters.

---
+++ Chessboard.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList
+
+class ChessBoard() {
+
+    private final static int BOARD_MAX = 8;
+    ArrayList<BoardTile> chessBoard = new ArrayList<BoardTile>();
+    private String colourPreset[] = ["b" , "w"];
+
+    // Set 8 by 8 chess board. 
+    private static void initializeBoard() {
+        String boardTileColour;
+        for (int i = 0; i < BOARD_MAX, i++) {
+            for (int j = 0; j < BOARD_MAX, j++) {
+                boardTileColour = colourPreset[j % 2];
+                BoardTile newBoardTile = new BoardTile(i, j, boardTileColour)
+                chessBoard.add(newBoardTile);
+            }
+        }
+    }
+}
+
+class BoardTile() {
+
+    private int xPos;
+    private int ypos;
+    private char colour;
+
+    public BoardTile(int xPos, int yPos, char colour) {
+        setxPos(xPos);
+        setyPos(yPos);
+        setColour(colour);
+    }
+
+    public void setxPos(int xPosToSet) {
+        this.xPos = xPosToSet;
+    }
+
+    public int getxPos() {
+        tempxPos = this.xPos;
+        return tempxPos;
+    }
+
+    public void setyPos(int yPosToSet) {
+        this.yPos = yPosToSet;
+    }
+
+    public int getyPos() {
+        tempyPos = this.yPos;
+        return tempyPos;
+    }
+
+    public void setColour(int colourToSet) {
+        this.xPos = xPosToSet;
+    }
+
+    public char getColour() {
+        tempColour = this.colour;
+        return tempColour;
+    }
+}
