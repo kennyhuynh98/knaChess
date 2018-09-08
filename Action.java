2b826c4553c24ef0264f4108741d395dfc4b209d
Kenny H <kennyhuynh1198@gmail.com>
2018-09-09 05:26:46 +1000

    Action class to hold actions in a list?
    **Not in use***
    
    Action:
    - Compile the Piece's possible actions in an actions list.

---
+++ Action.java
@@ -0,0 +1,20 @@
+// import java.util.ArrayList;
+
+// public class Action<T> {
+
+//     public Piece currentPiece;
+//     public BoardTile indicatedBoardTile;
+//     public Piece targetPiece;
+
+//     public Action(Piece piece, BoardTile tileToMove, Piece targetPiece) {
+//         this.currentPiece = piece;
+//         this.indicatedBoardTile = tileToMove;
+//         this.targetPiece = targetPiece;
+//     }
+
+//     public void displayActions(ArrayList<T> currentPieceActionList) {
+//         foreach (<T> currentAction : currentPieceActionList) {
+//             System.out.println(currentAction.currentPiece);
+//         }
+//     }
+// }
